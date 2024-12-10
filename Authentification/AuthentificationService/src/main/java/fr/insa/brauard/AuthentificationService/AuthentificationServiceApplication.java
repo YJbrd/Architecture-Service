72f39b41d4d490a40b1e14f9156fb9e38271a5cd
Brugiere Nathan <brugiere@insa-20930>
2024-12-10 17:50:35 +0100

    Add services

---
+++ Authentification/AuthentificationService/src/main/java/fr/insa/brauard/AuthentificationService/AuthentificationServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.brauard.AuthentificationService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuthentificationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AuthentificationServiceApplication.class, args);
+	}
+
+}
---
+++ ConnexionBDD/BDDConnexionService/src/main/java/fr/insa/brauard/BDDConnexionService/BddConnexionServiceApplication.java
@@ -0,0 +1,13 @@
+package fr.insa.brauard.BDDConnexionService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddConnexionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddConnexionServiceApplication.class, args);
+	}
+
+}
