6279d3dfb67783535301eaa7380734a70f2a2677
Nathan <nathan.b2470@gmail.com>
2024-12-21 18:02:55 +0100

    Update AuthentificationServiceApplication.java

--- Authentification/AuthentificationService/src/main/java/fr/insa/brauard/AuthentificationService/AuthentificationServiceApplication.java
+++ Authentification/AuthentificationService/src/main/java/fr/insa/brauard/AuthentificationService/AuthentificationServiceApplication.java
@@ -9,7 +9,6 @@ import org.springframework.web.client.RestTemplate;
 
 
 @SpringBootApplication
-@ComponentScan(basePackages = {"fr.insa.brauard.AuthentificationService", "fr.insa.brauard.BDDConnexionService", "fr.insa.brauard.RequestService"})
 public class AuthentificationServiceApplication {
 	
 	@Bean
